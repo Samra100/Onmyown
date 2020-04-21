eabfd074f268118dc587a59e294559fce9f8a8bb
Samra Mekonnen <smekonnen53@gmail.com>
2020-04-21 16:09:41 -0700

    first commit

---
+++ src/com/samra/Main.java
@@ -0,0 +1,17 @@
+package com.samra;
+
+public static class bigDiff{
+
+    public static int bigDiff(int[] nums) {
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        return max - min;
+    }
+}
+
---
+++ src/com/samra/commonEnd.java
@@ -0,0 +1,23 @@
+package com.samra;
+//conditional statement
+//either same first element OR same last element
+//Both arrays will be length 1 OR more
+
+
+
+public class commonEnd {
+
+    public boolean commonEnd(int[] a, int[] b) {
+        int num1 = 0;
+        int num2 = 0;
+
+        if (a[0] == b[0]) || a[a.length - 1] == b[b.length - 1]{
+
+        }
+        return true;
+
+        else
+
+        return false;
+    }
+}
